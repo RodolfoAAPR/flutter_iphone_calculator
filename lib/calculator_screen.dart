dd35231196686b90087169ac2d77ab24dfb1de84
Rodolfo Alves <165041838+RodolfoAAPR@users.noreply.github.com>
2026-03-11 19:50:05 -0300

    feat(calculator_screen): add numbers alignment and buttons padding

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -12,20 +12,27 @@ class CalculatorScreenState extends State<CalculatorScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
-      child: Column(
-        children: [
-          Expanded(
-            flex: 4,
-            child: Text(
-              "123456789", 
-            style: TextStyle(fontSize: 70,
-             decoration: TextDecoration.none,
-              color: Colors.white)),
-          ),
-          Expanded(
-            flex: 6,
-            child: Placeholder())
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Container(
+              child: Expanded(
+                flex: 4,
+                child: Container(alignment: Alignment.bottomRight,
+                  child: Text(
+                    "123456789", 
+                  style: TextStyle(fontSize: 70,
+                   decoration: TextDecoration.none,
+                    color: Colors.white)),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Placeholder())
+          ],
+        ),
       )
     );
   }
