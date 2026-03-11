00a0f8d1c8c157495894fcb031af0b2a13242035
Rodolfo Alves <165041838+RodolfoAAPR@users.noreply.github.com>
2026-03-11 20:31:28 -0300

    feat(calculator_screen): add numbers button

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -19,21 +19,91 @@ class CalculatorScreenState extends State<CalculatorScreen> {
             Container(
               child: Expanded(
                 flex: 4,
-                child: Container(alignment: Alignment.bottomRight,
+                child: Container(
+                  alignment: Alignment.bottomRight,
                   child: Text(
-                    "123456789", 
-                  style: TextStyle(fontSize: 70,
-                   decoration: TextDecoration.none,
-                    color: Colors.white)),
+                    "123456789",
+                    style: TextStyle(
+                      fontSize: 70,
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
             Expanded(
               flex: 6,
-              child: Placeholder())
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        getButton("AC"),
+                        getButton("()"),
+                        getButton("%"),
+                        getButton("/"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        getButton("7"),
+                        getButton("8"),
+                        getButton("9"),
+                        getButton("/"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        getButton("AC"),
+                        getButton("()"),
+                        getButton("%"),
+                        getButton("/"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        getButton("AC"),
+                        getButton("()"),
+                        getButton("%"),
+                        getButton("/"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        getButton("AC"),
+                        getButton("()"),
+                        getButton("%"),
+                        getButton("/"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-      )
+      ),
+    );
+  }
+
+  Widget getButton(String text) {
+    return Expanded(
+      flex: 1,
+      child: FloatingActionButton(onPressed: () {}, child: Text(text)),
     );
   }
 }
